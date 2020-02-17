88e730cfdc056f492e3a3d9e98141244a941fcfe
frogix <frogix@ya.ru>
2020-02-17 22:54:04 +0500

    added input form

--- KnapsackProblem/Form1.Designer.cs
+++ KnapsackProblem/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.inputDG = new System.Windows.Forms.DataGridView();
+            this.thingsNumberInput = new System.Windows.Forms.NumericUpDown();
+            this.solveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thingsNumberInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputDG
+            // 
+            this.inputDG.AllowUserToAddRows = false;
+            this.inputDG.AllowUserToDeleteRows = false;
+            this.inputDG.AllowUserToResizeColumns = false;
+            this.inputDG.AllowUserToResizeRows = false;
+            this.inputDG.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.inputDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inputDG.ColumnHeadersVisible = false;
+            this.inputDG.Location = new System.Drawing.Point(59, 111);
+            this.inputDG.Name = "inputDG";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.inputDG.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.inputDG.RowHeadersWidth = 100;
+            this.inputDG.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Format = "N0";
+            dataGridViewCellStyle2.NullValue = null;
+            this.inputDG.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.inputDG.Size = new System.Drawing.Size(643, 70);
+            this.inputDG.TabIndex = 0;
+            this.inputDG.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.InputDG_EditingControlShowing);
+            // 
+            // thingsNumberInput
+            // 
+            this.thingsNumberInput.Location = new System.Drawing.Point(264, 28);
+            this.thingsNumberInput.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.thingsNumberInput.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.thingsNumberInput.Name = "thingsNumberInput";
+            this.thingsNumberInput.Size = new System.Drawing.Size(120, 20);
+            this.thingsNumberInput.TabIndex = 1;
+            this.thingsNumberInput.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.thingsNumberInput.ValueChanged += new System.EventHandler(this.thingsNumberInput_ValueChanged);
+            // 
+            // solveBtn
+            // 
+            this.solveBtn.Location = new System.Drawing.Point(486, 25);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(75, 23);
+            this.solveBtn.TabIndex = 2;
+            this.solveBtn.Text = "Решить";
+            this.solveBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 229);
+            this.Controls.Add(this.solveBtn);
+            this.Controls.Add(this.thingsNumberInput);
+            this.Controls.Add(this.inputDG);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.inputDG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thingsNumberInput)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView inputDG;
+        private System.Windows.Forms.NumericUpDown thingsNumberInput;
+        private System.Windows.Forms.Button solveBtn;
     }
 }
 
--- KnapsackProblem/Form1.cs
+++ KnapsackProblem/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KnapsackProblem
@@ -15,6 +8,183 @@ namespace KnapsackProblem
         public Form1()
         {
             InitializeComponent();
+
+            //solveBtn.Enabled = false;
+
+            InitializeTable();
+
+            UpdateTable();
+            ClearTable();
+        }
+
+        public void InitializeTable ()
+        {
+            inputDG.Columns.Add("header", "");
+            var rowHeaders = new string[]{ "Номер", "масса", "стоимость" };
+
+            foreach (var headerText in rowHeaders)
+            {
+                var row = new DataGridViewRow();
+                var headerCell = new DataGridViewRowHeaderCell();
+
+                headerCell.Value = headerText;
+                row.HeaderCell = headerCell;
+
+                inputDG.Rows.Add(row);
+            }
+
+            string[] vals = new string[thingsNumber];
+            for (int i = 0; i < thingsNumber; i++)
+            {
+                vals[i] = (i + 1).ToString();
+            }
+            inputDG.Rows[0].SetValues(vals);
+        }
+
+        // Число вершин (равно инпуту)
+        public int thingsNumber
+        {
+            get => (int)thingsNumberInput.Value;
+            set => thingsNumberInput.Value = value;
+        }
+
+        // True  = заполнены все ячейки таблицы
+        // False = хотя бы одна не заполнена
+        public bool IsDataFilled
+        {
+            get
+            {
+                foreach (DataGridViewRow row in inputDG.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        if (cell.Value == null) return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        // Данные из таблицы -> двумерный массив интов
+        public int[,] ParseData()
+        {
+            if (!IsDataFilled) throw new Exception("Данные не заполнены");
+
+            var matrix = new int[3, thingsNumber];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < thingsNumber; j++)
+                {
+                    var cell = inputDG.Rows[i].Cells[j];
+
+                    matrix[i, j] = int.Parse(cell.Value.ToString());
+                }
+            }
+
+            return matrix;
+        }
+
+        // Подстраивает таблицу под новое число вершин, добавляя или удаляя столбцы
+        public void UpdateTable()
+        {
+            int prevColsNumber = inputDG.Columns.Count;
+
+            int delta = thingsNumber - prevColsNumber;
+
+            if (delta > 0)
+            {
+                for (int i = prevColsNumber; i < thingsNumber; i++)
+                {
+                    inputDG.Columns.Add("col" + (i+1).ToString(), (i+1).ToString());
+                    inputDG.Columns[i].Width = 50;
+
+                    var numberCell = inputDG[i, 0];
+                    numberCell.Value = (i + 1).ToString();
+
+                }
+            }
+            else
+            {
+                for (int i = prevColsNumber - 1; i >= thingsNumber; i--)
+                {
+                    inputDG.Columns.RemoveAt(i);
+                }
+
+            }
+
+            // Возможно, что при уменьшении размера таблицы не осталось незаполненных вершин
+            // тогда кнопка должна стать активной
+            //solveBtn.Enabled = IsDataFilled;
+        }
+
+        public void ClearTable()
+        {
+            for (int i = 0; i < inputDG.Columns.Count; i++)
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    inputDG[i,j].Value = 0;
+                }
+            }
+
+            //solveBtn.Enabled = false;
+        }
+
+        // Изменение числа вершин
+        private void VerticesNumberInput_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateTable();
+        }
+
+        // На каждую ячейку вешается фильтрующий ввод обработчик события
+        private void InputDG_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Column_KeyPress);
+            TextBox tb = e.Control as TextBox;
+            if (tb != null)
+            {
+                tb.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+            }
+        }
+
+        // Фильтрация ввода текста в ячейку таблицы (только цифры)
+        private void Column_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var activeCell = inputDG.SelectedCells[0];
+
+            if (activeCell.RowIndex == 0 ||                             // строка с номером - readOnly
+                !char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) // только цифры
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        // Проверка заполненности формы по завершение редактирования ячейки таблицы
+        private void InputDG_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            //solveBtn.Enabled = IsDataFilled;
+        }
+
+
+
+        private void SolveBtn_Click(object sender, EventArgs e)
+        {
+            int[,] matrix = ParseData();
+            // Тут передача данных в следующую форму
+
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+        }
+
+        private void thingsNumberInput_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateTable();
         }
     }
 }
